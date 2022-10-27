bcfd0fe18d03ee32c34545f4413d6133cf83bc66
James Ishaku <66888791+Jamesishaku@users.noreply.github.com>
2022-10-27 20:52:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="profile">
+        <img src={require('./images/photo.JPG')} alt="James Ishaku photo" id="profile__img"/>
+        <h3 id="twitter">James🎯</h3>
+        <p id="slack">js_nerd</p>
       </header>
+      <main>
+          <a href="https://twitter.com/js_nerd">Twitter Link</a>
+          <a href="https://training.zuri.team/" id="btn__zuri">Zuri Team</a>
+          <a href="https://books.zuri.team" id="books">Zuri Books</a>
+          <a href="https://books.zuri.team/python-for-beginners?ref_id=js_nerd" id="book__python">Python Book</a>
+          <a href="https://background.zuri.team" id="pitch">Background Check for Coders</a>
+          <a href="https://books.zuri.team/design-rules" id="book__design">Design Books</a>
+      </main>
+      <section className="social-section">
+      <img src={require("./images/slack.png")} alt="slack icon"/>
+      <img src={require("./images/github.png")} alt="github icon"/>
+      </section>
+      <footer>
+        <h4>Zuri<span>.</span>Internship</h4>
+        <p>HNG Internship 9 Frontend Task</p>
+        <img src={require("./images/I4G.png")} alt="I4G logo"/>
+      </footer>
     </div>
   );
 }
