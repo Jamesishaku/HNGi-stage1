6f3d670ccfbddec740d2fb64b1e5cc3a55c79c9e
James Ishaku <66888791+Jamesishaku@users.noreply.github.com>
2022-11-04 08:48:04 +0100

    feat: added form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,23 +11,23 @@ export default function Contact(props) {
                 <div className="form-group multiple">
                     <div className="input-group">
                         <label htmlFor="first_name">Full name</label>
-                        <input type="text" placeholder="Enter your first name" id="first_name"/>
+                        <input type="text" placeholder="Enter your first name" id="first_name" required/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="last_name">Last name</label>
-                        <input type="text" placeholder="Enter your last name" id="last_name"/>
+                        <input type="text" placeholder="Enter your last name" id="last_name" required/>
                     </div>
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" placeholder="yourname@email.com" id="email"/>
+                    <input type="email" placeholder="yourname@email.com" id="email" required/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="message">Message</label>
-                    <textarea placeholder="Send me a message and I'll reply you as soon as possible... " id="message"></textarea>
+                    <textarea placeholder="Send me a message and I'll reply you as soon as possible... " id="message" required></textarea>
                 </div>
                 <div className="form-group">
-                   <input type="checkbox" id="agreement"/> <label htmlFor="agreement" className="agreement"> &nbsp; You agree to providing your data to {props.name} who may contact you.</label>
+                   <input type="checkbox" id="agreement" required/> <label htmlFor="agreement" className="agreement"> &nbsp; You agree to providing your data to {props.name} who may contact you.</label>
                 </div>
                 <div className="form-group">
                     <input type="submit" value="Send message" id="btn_submit"/>
